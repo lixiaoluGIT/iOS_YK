1e2a94666dd520fdd7809d908395341879423260
LXL <755160804@qq.com>
2018-03-13 19:03:14 +0800

    tijao

--- YK/Public/YKMainVC.m
+++ YK/Public/YKMainVC.m
@@ -13,7 +13,10 @@
 #import "YKMineVC.h"
 #import "YKHomeSegementVC.h"
 
-@interface YKMainVC ()
+@interface YKMainVC ()<UITabBarControllerDelegate>
+{
+    NSInteger _currentIndex;
+}
 
 @end
 
@@ -82,7 +85,8 @@
 
         self.viewControllers = @[home, near, meaaage, list];
         
-        
+        self.delegate = self;
+        self.selectedIndex = 0;
 //        [[UITabBar appearance] setBarTintColor:[UIColor whiteColor]];
         
         for (UITabBarItem *item in self.tabBar.items) {
@@ -100,4 +104,55 @@
     
 }
 
+- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController{
+    if (tabBarController.selectedIndex == 3) {
+        
+    }
+    
+    //点击tabBarItem动画
+    if (self.selectedIndex != _currentIndex)[self tabBarButtonClick:[self getTabBarButton]];
+    
+    
+}
+- (UIControl *)getTabBarButton{
+    NSMutableArray *tabBarButtons = [[NSMutableArray alloc]initWithCapacity:0];
+    
+    /*
+     "<_UITabBarBackgroundView: 0x7fddb21236e0; frame = (0 0; 375 49); autoresize = W; userInteractionEnabled = NO; layer = <CALayer: 0x7fddb21297d0>>",
+     "<UITabBarButton: 0x7fddb23bb500; frame = (2 1; 90 48); opaque = NO; layer = <CALayer: 0x7fddb2130880>>",
+     "<UITabBarButton: 0x7fddb217e1a0; frame = (96 1; 90 48); opaque = NO; layer = <CALayer: 0x7fddb217eec0>>",
+     "<UITabBarButton: 0x7fddb2184700; frame = (190 1; 89 48); opaque = NO; layer = <CALayer: 0x7fddb2184e20>>",
+     "<UITabBarButton: 0x7fddb21893c0; frame = (283 1; 90 48); opaque = NO; layer = <CALayer: 0x7fddb2189b60>>",
+     "<UIImageView: 0x7fddb217ea70; frame = (0 -0.5; 375 0.5); autoresize = W; userInteractionEnabled = NO; layer = <CALayer: 0x7fddb219fa40>>"
+     */
+    for (UIView *tabBarButton in self.tabBar.subviews) {
+        if ([tabBarButton isKindOfClass:NSClassFromString(@"UITabBarButton")]){
+            [tabBarButtons addObject:tabBarButton];
+        }
+    }
+    UIControl *tabBarButton = [tabBarButtons objectAtIndex:self.selectedIndex];
+    
+    return tabBarButton;
+}
+- (void)tabBarButtonClick:(UIControl *)tabBarButton
+{
+    /*
+     "<UITabBarSwappableImageView: 0x7fd7ebc52760; frame = (32 5.5; 25 25); opaque = NO; userInteractionEnabled = NO; tintColor = UIDeviceWhiteColorSpace 0.572549 1; layer = <CALayer: 0x7fd7ebc52940>>",
+     "<UITabBarButtonLabel: 0x7fd7ebc4f360; frame = (29.5 35; 30 12); text = '\U8d2d\U7269\U8f66'; opaque = NO; userInteractionEnabled = NO; layer = <_UILabelLayer: 0x7fd7ebc4e090>>" a
+     */
+    for (UIView *imageView in tabBarButton.subviews) {
+        if ([imageView isKindOfClass:NSClassFromString(@"UITabBarSwappableImageView")]) {
+            //需要实现的帧动画,这里根据需求自定义
+            CAKeyframeAnimation *animation = [CAKeyframeAnimation animation];
+            animation.keyPath = @"transform.scale";
+            animation.values = @[@1.0,@1.1,@0.9,@1.0];
+            animation.duration = 0.3;
+            animation.calculationMode = kCAAnimationCubic;
+            //把动画添加上去就OK了
+            [imageView.layer addAnimation:animation forKey:nil];
+        }
+    }
+    
+    _currentIndex = self.selectedIndex;
+}
 @end
