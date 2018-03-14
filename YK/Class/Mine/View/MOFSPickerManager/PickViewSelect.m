5fa48f0a7dd59e2c3474bc5a19f296e07e6108b8
LXL <755160804@qq.com>
2018-03-14 14:34:11 +0800

    update today's的task

--- YK/Class/Mine/View/MOFSPickerManager/PickViewSelect.m
+++ YK/Class/Mine/View/MOFSPickerManager/PickViewSelect.m
@@ -73,7 +73,7 @@
         [pickerData addObject:loString];
     }
     
-   NSMutableArray *pickerHour=[NSMutableArray arrayWithObjects:@"10:00-11:00",@"11:00-12:00",@"12:00-13:00",@"13:00-14:00",@"14:00-15:00",@"15:00-16:00",@"16:00-17:00",@"17:00-18:00",@"18:00-19:00",@"19:00-20:00", nil];
+   NSMutableArray *pickerHour=[NSMutableArray arrayWithObjects:@"8:00-9:00",@"09:00-10:00",@"10:00-11:00",@"11:00-12:00",@"12:00-13:00",@"13:00-14:00",@"14:00-15:00",@"15:00-16:00", nil];
     self.pickDate = pickerData;
     self.pickHour = pickerHour;
 }
